a1e03ab5cdd1e07ab93eea37fa935ac740c018d9
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-11-13 23:31:43 -0300

    feat(login): Add google sign in button

--- app/src/main/java/com/newhome/app/LoginActivity.kt
+++ app/src/main/java/com/newhome/app/LoginActivity.kt
@@ -1,15 +1,25 @@
-package com.newhome
+package com.newhome.app
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.newhome.dto.Credenciais
-import com.newhome.utils.DialogDisplayer
-import com.newhome.utils.LoadingDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.newhome.app.R
+import com.newhome.app.dto.Credenciais
+import com.newhome.app.utils.DialogDisplayer
+import com.newhome.app.utils.LoadingDialog
 import kotlinx.coroutines.launch
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginLoginText: EditText
@@ -17,6 +27,10 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var cadastrarLoginButton: Button
     private lateinit var fazerLoginLoginButton: Button
+    private lateinit var googleSignInButton: Button
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
 
     private val dialog = LoadingDialog(this)
     private lateinit var dialogDisplayer: DialogDisplayer
@@ -33,9 +47,19 @@ class LoginActivity : AppCompatActivity() {
 
         cadastrarLoginButton = findViewById(R.id.cadastrarLoginButton)
         fazerLoginLoginButton = findViewById(R.id.fazerLoginLoginButton)
+        googleSignInButton = findViewById(R.id.googleSignInButton)
 
         fazerLoginLoginButton.setOnClickListener { lifecycleScope.launch { onFazerLogin() } }
         cadastrarLoginButton.setOnClickListener { onCadastrar() }
+        googleSignInButton.setOnClickListener { onGoogleSignIn() }
+
+        configureGoogleSignIn()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        checkGoogleSignedIn()
     }
 
     override fun onPause() {
@@ -88,5 +112,45 @@ class LoginActivity : AppCompatActivity() {
 
         val intent = Intent(applicationContext, CriarContaActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun configureGoogleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        googleSignInLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                //if (result.resultCode != RESULT_OK) return@registerForActivityResult
+
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleSignInResult(task)
+            }
+    }
+
+    private fun checkGoogleSignedIn() {
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) onSignedIn(account)
+    }
+
+    private fun onGoogleSignIn() {
+        googleSignInLauncher.launch(googleSignInClient.signInIntent)
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            onSignedIn(account)
+        } catch (e: ApiException) {
+            e.printStackTrace()
+            dialogDisplayer.display("Falha ao fazer login", e)
+        }
+    }
+
+    private fun onSignedIn(account: GoogleSignInAccount) {
+        // TODO ja ta logado
+        dialogDisplayer.display("Logado com sucesso ${account.email}")
     }
 }
